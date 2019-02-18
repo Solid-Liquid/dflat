9e8b7e32d3937bc2ff4c0fce87c45231457e5f58
AvoHagopian <avedis.hagopian.111@my.csun.edu>
2019-02-17 20:56:46 -0800

    Class names for each token

--- src/token.hpp
+++ src/token.hpp
@@ -4,11 +4,127 @@
 namespace dflat
 {
 
-class Token
-{
-    public:
-        Token();
-};
+    class Token
+    {
+        public:
+            Token();
+    };
+
+
+    class NumberToken
+    {
+        public:
+    };
+
+    class VariableToken
+    {
+        public:
+    };
+
+    class IfToken
+    {
+        public:
+    };
+
+    class ThenToken
+    {
+        public:
+    };
+
+    class ElseToken
+    {
+        public:
+    };
+
+    class PlusToken
+    {
+        public:
+    };
+
+    class MinusToken
+    {
+        public:
+    };
+
+    class MultiplyToken
+    {
+        public:
+    };
+
+    class DivisionToken
+    {
+        public:
+    };
+
+    class LogicalAndToken
+    {
+        public:
+    };
+
+    class LogicalOrToken
+    {
+        public:
+    };
+
+    class BangToken
+    {
+        public:
+    };
+
+    class LeftParenToken
+    {
+        public:
+    };
+
+    class RightParenToken
+    {
+        public:
+    };
+
+    class LeftBracketToken
+    {
+        public:
+    };
+
+    class RightBracketToken
+    {
+        public:
+    };
+
+    class LeftBraceToken
+    {
+        public:
+    };
+
+    class RightBraceToken
+    {
+        public:
+    };
+
+    class EqualToken
+    {
+        public:
+    };
+
+    class PointToken
+    {
+        public:
+    };
+
+    class ForToken
+    {
+        public:
+    };
+
+    class WhileToken
+    {
+        public:
+    };
+
+    class NewlineToken
+    {
+        public:
+    };
 
 } //namespace dflat
 
