4cd83d04f6394c6661928609d09540a2f3256a34
bmcclelland <brian.mcclelland.982@my.csun.edu>
2019-02-18 01:26:22 -0800

    made Token::getType const and inlined for brevity

--- src/token.cpp
+++ src/token.cpp
@@ -11,87 +11,13 @@ Token::~Token()
 //NumberToken:
 NumberToken::NumberToken(int num_)
     : num(num_)
-{}
-
-TokType NumberToken::getType()
 {
-    return tokNum;
 }
 
 //VariableToken:
 VariableToken::VariableToken(String const& name_)
     : name(name_)
-{}
-
-TokType VariableToken::getType()
 {
-    return tokVar;
-}
-
-//FunctionToken:
-TokType FunctionToken::getType()
-{
-    return tokFunc;
-}
-
-//IfToken:
-TokType IfToken::getType()
-{
-    return tokIf;
-}
-
-//ElseToken:
-TokType ElseToken::getType()
-{
-    return tokElse;
-}
-
-//PlusToken:
-TokType PlusToken::getType()
-{
-    return tokPlus;
-}
-
-//MinusToken:
-TokType MinusToken::getType()
-{
-    return tokMinus;
-}
-
-//DivisionToken:
-TokType DivisionToken::getType()
-{
-    return tokDiv;
-}
-
-//MultiplyToken:
-TokType MultiplyToken::getType()
-{
-    return tokMult;
-}
-
-//LeftBraceToken:
-TokType LeftBraceToken::getType()
-{
-    return tokLBrace;
-}
-
-//RightBraceToken:
-TokType RightBraceToken::getType()
-{
-    return tokRBrace;
-}
-
-//LeftParenToken:
-TokType LeftParenToken::getType()
-{
-    return tokLParen;
-}
-
-//RightBraceToken:
-TokType RightParenToken::getType()
-{
-    return tokRParen;
 }
 
 } //namespace dflat
--- src/token.hpp
+++ src/token.hpp
@@ -12,7 +12,7 @@ namespace dflat
     {
     public:
         virtual ~Token();
-        virtual TokType getType() = 0;
+        virtual TokType getType() const = 0;
     };
 
 
@@ -20,7 +20,7 @@ namespace dflat
     {
     public:
         NumberToken(int);
-        TokType getType();
+        TokType getType() const { return tokNum; }
         int num;
     };
 
@@ -28,134 +28,134 @@ namespace dflat
     {
     public:
         VariableToken(String const&);
-        TokType getType();
+        TokType getType() const { return tokVar; }
         String name;
     };
 
     class FunctionToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokFunc; }
     };
 
     class IfToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokIf; }
     };
 
     class ElseToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokElse; }
     };
 
     class PlusToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokPlus; }
     };
 
     class MinusToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokMinus; }
     };
 
     class MultiplyToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokMult; }
     };
 
     class DivisionToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokDiv; }
     };
 
     class LeftBraceToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokLBrace; }
     };
 
     class RightBraceToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokRBrace; }
     };
 
     class LeftParenToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokLParen; }
     };
 
     class RightParenToken : public Token
     {
     public:
-        TokType getType();
+        TokType getType() const { return tokRParen; }
     };
 
 //    class LogicalAndToken : public Token
 //    {
 //    public:
-//        TokType getType();
+//        TokType getType() const { return tok; }
 //    };
 
 //    class LogicalOrToken : public Token
 //    {
 //    public:
-//        TokType getType();
+//        TokType getType() const { return tok; }
 //    };
 
 //    class BangToken : public Token//!
 //    {
 //    public:
-//        TokType getType();
+//        TokType getType() const { return tok; }
 //    };
 
 //    class LeftBracketToken : public Token
 //    {
 //    public:
-//        TokType getType();
+//        TokType getType() const { return tok; }
 //    };
 
 //    class RightBracketToken : public Token
 //    {
 //    public:
-//        TokType getType();
+//        TokType getType() const { return tok; }
 //    };
 
 //    class EqualToken : public Token
 //    {
 //    public:
-//        TokType getType();
+//        TokType getType() const { return tok; }
 //    };
 
 //    class PointToken : public Token
 //    {
 //    public:
-//        TokType getType();
+//        TokType getType() const { return tok; }
 //    };
 
 //    class ForToken : public Token
 //    {
 //    public:
-//        TokType getType();
+//        TokType getType() const { return tok; }
 //    };
 
 //    class WhileToken : public Token
 //    {
 //    public:
-//        TokType getType();
+//        TokType getType() const { return tok; }
 //    };
 
 //    class NewlineToken : public Token
 //    {
 //    public:
-//        TokType getType();
+//        TokType getType() const { return tok; }
 //    };
 
 } //namespace dflat
