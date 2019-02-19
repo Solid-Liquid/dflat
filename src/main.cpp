2e2369e470a5f410abe79261fff13c2424837420
Solid-Liquid <Solid-Liquid@users.noreply.github.com>
2019-02-18 23:22:48 -0800

    Updated main to run tokenize on file

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,6 @@ using namespace dflat;
 
 int main(int argc, char* argv[])
 {
-	//tokenize("bunch o shit");
-
-    //TOKEN USE EXAMPLE
-    Token* tk = new NumberToken(3); //store a number token in a base class token pointer
-
-    //try to get as desired type
-    if (auto tkNum = tk->as<NumberToken>())
-    {
-        tkNum->num = 5;
-        String test = tkNum->toString();
-        cout << "Quick test (delete later): " << test << " ";
-    }
-    Token* point = new LeftBraceToken();
-    cout << point->toString() << endl;
-    delete tk;
-    delete point;
-    //END TOKEN USE EXAMPLE
-
     string fileName;
 
     if(argc == 2)
@@ -58,7 +40,11 @@ int main(int argc, char* argv[])
     }
 
     ///DO STUFF WITH FILE CONTENTS
-    cout << fileContents << endl;
+    Vector<TokenPtr> tokens = tokenize(fileContents); //run lexer
+    for(unsigned long long i=0; i<tokens.size(); ++i)
+    {
+        cout << tokens[i]->toString() << endl;
+    }
 
     return 0;
 }
