de37125973f872366c34ca64e268ea3acc722b2c
Brian McClelland <47439126+bmcclelland@users.noreply.github.com>
2019-02-18 23:41:08 -0800

    added skipWhitespace and lookupKeyword to Lexer

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,8 +1,9 @@
 #include "lexer.hpp"
+#include "map.hpp"
 
 namespace dflat
 {
-
+    using std::make_unique;
 
 class Lexer : private LexerCore
 {
@@ -10,11 +11,50 @@ class Lexer : private LexerCore
         Lexer(String const&);
         Vector<TokenPtr> tokenize();
         //TokenPtr singleToken();
+        void skipWhitespace();
+        TokenPtr lookupKeyword(String const&);
 };
 
 Lexer::Lexer(String const& input)
     : LexerCore(input)
 {
+}
+
+TokenPtr Lexer::lookupKeyword(String const& name)
+{
+    static Map<String, TokType> const kws
+    {
+        { "if", tokIf },
+        { "else", tokElse },
+        { "for", tokFor },
+        { "while", tokWhile },
+    };
+
+    auto it = kws.find(name);
+
+    if (it == kws.end())
+    {
+        return nullptr;
+    }
+
+    switch (it->second)
+    {
+        case tokIf: return make_unique<IfToken>();
+        case tokElse: return make_unique<ElseToken>();
+        case tokFor: return make_unique<ForToken>();
+        case tokWhile: return make_unique<WhileToken>();
+        default: return nullptr;
+    }
+}
+
+void Lexer::skipWhitespace()
+{
+    char c = peek();
+
+    while (c == ' ' || c == '\t') // no \n
+    {
+        c = get();
+    }
 }
 
 // TokenPtr Lexer::singleToken(){
