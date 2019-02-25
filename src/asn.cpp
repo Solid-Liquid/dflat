47119d562f7670105b6f3f4a30495974c9b8e0d3
Solid-Liquid <Solid-Liquid@users.noreply.github.com>
2019-02-25 01:31:47 -0800

    Wrote BinopExp

--- src/asn.cpp
+++ src/asn.cpp
@@ -3,8 +3,38 @@
 namespace dflat
 {
 
+String opString(OpType op)
+{
+    switch(op)
+    {
+        case opPlus: return "+";
+        case opMinus: return "-";
+        case opDiv: return "/";
+        case opMult: return "*";
+        case opAnd: return "&&";
+        case opOr: return "||";
+        case opLogEqual: return "==";
+        case opLogNotEq: return "!=";
+        default: return "oopsie";
+    }
+}
+
+
+//ASN:
 ASN::~ASN()
 {
+}
+
+//BinopExp:
+BinopExp::BinopExp(ASN_Ptr&& _left, OpType _op, ASN_Ptr&& _right)
+    :left(std::move(_left)),right(std::move(_right)),op(_op)
+{
+}
+
+String BinopExp::toString()
+{
+    return "(" + left->toString() + " " + opString(op) +
+            " " + right->toString() + ")";
 }
 
 } //namespace dflat
