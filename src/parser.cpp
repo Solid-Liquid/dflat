3fe29bb091e0943ecf53503d9faf6814abd5ebb3
Brian McClelland <47439126+bmcclelland@users.noreply.github.com>
2019-02-25 18:09:57 -0800

    more parser parts

--- src/parser.cpp
+++ src/parser.cpp
@@ -50,26 +50,34 @@ class Parser
     //              and the current token advances.
     //  On failure, the present function returns early with nullptr.
     #define MATCH(var, type) \
-    type const* var##__ = match<type>(); \
-    if (!var##__) { return nullptr; } \
-    type const& var = *var##__ \
-    /*end MATCH*/
+        type const* var##__ = match<type>(); \
+        if (!var##__) { return nullptr; } \
+        type const& var = *var##__ \
+        /*end MATCH*/
+    
+    // Matches the current token but doesn't store it in a var.
+    #define MATCH_(type) \
+        if (!match<type>()) { return nullptr; } \
+        /*end MATCH_*/
 
-    ASNPtr parseVariable()
+    Optional<OpType> parseMultiveOp()
     {
-        MATCH(var, VariableToken);
-        return make_unique<VariableExp>(var.name);
+        switch (cur()->getType())
+        {
+            case tokMult:   return opMult;
+            case tokDiv:    return opDiv;
+            default:        return failure;
+        }
     }
     
-    ASNPtr parseNumber()
+    Optional<OpType> parseAdditiveOp()
     {
-        MATCH(num, NumberToken);
-        return make_unique<NumberExp>(num.num);
-    }
-
-    ASNPtr parseAdditive()
-    {
-        return nullptr; //TODO
+        switch (cur()->getType())
+        {
+            case tokPlus:   return opPlus;
+            case tokMinus:  return opMinus;
+            default:        return failure;
+        }
     }
 
     Optional<OpType> parseLogicalOp()
@@ -82,6 +90,137 @@ class Parser
             case tokNotEq:  return opLogNotEq;
             default:        return failure;
         }
+    }
+    
+    ASNPtr parseVariable()
+    {
+        MATCH(var, VariableToken);
+        return make_unique<VariableExp>(var.name);
+    }
+    
+    ASNPtr parseNumber()
+    {
+        MATCH(num, NumberToken);
+        return make_unique<NumberExp>(num.num);
+    }
+
+    ASNPtr parseUnary()
+    {
+        return nullptr; //TODO
+    }
+    
+    ASNPtr parseMethodCall()
+    {
+        return nullptr; //TODO
+    }
+    
+    ASNPtr parseNew()
+    {
+        return nullptr; //TODO
+    }
+
+    ASNPtr parseParensExp()
+    {
+        //TODO rollback
+        MATCH_(LeftParenToken);
+        
+        ASNPtr exp = parseExp();
+
+        if (!exp)
+        {
+            return nullptr;
+        }
+
+        MATCH_(RightParenToken);
+        return exp;
+    }
+
+    ASNPtr parsePrimary()
+    {
+        ASNPtr result;
+        
+        if (result = parseParensExp())
+        {
+            return result;
+        }
+        else if (result = parseUnary())
+        {
+            return result;
+        }
+        else if (result = parseMethodCall())
+        {
+            return result;
+        }
+        else if (result = parseNew())
+        {
+            return result;
+        }
+        else if (result = parseVariable())
+        {
+            return result;
+        }
+        else if (result = parseNumber())
+        {
+            return result;
+        }
+        else
+        {
+            return nullptr;
+        }
+    }
+
+    ASNPtr parseMultive()
+    {
+        //TODO rollback
+        ASNPtr left = parsePrimary();
+
+        if (!left)
+        {
+            return nullptr;
+        }
+
+        Optional<OpType> op = parseMultiveOp();
+
+        if (!op)
+        {
+            return nullptr;
+        }
+
+        ASNPtr right = parseMultive();
+
+        if (!right)
+        {
+            return nullptr;
+        }
+
+        return make_unique<BinopExp>(move(left), *op, move(right));
+    }
+    
+    ASNPtr parseAdditive()
+    {
+        //TODO rollback
+        ASNPtr left = parseMultive();
+
+        if (!left)
+        {
+            return nullptr;
+        }
+
+        Optional<OpType> op = parseAdditiveOp();
+
+        if (!op)
+        {
+            return nullptr;
+        }
+
+        ASNPtr right = parseAdditive();
+
+        if (!right)
+        {
+            return nullptr;
+        }
+
+        return make_unique<BinopExp>(move(left), *op, move(right));
     }
 
     ASNPtr parseLogical()
@@ -111,22 +250,15 @@ class Parser
         return make_unique<BinopExp>(move(left), *op, move(right));
     }
 
-//    ASNPtr parseUnary()
-//    {
-//        PARSE(op, parseUnaryOp());
-//        PARSE(primary, parsePrimary());
-//        return make_unique<UnaryExp>(op, primray);
-//    }
-    
     ASNPtr parseExp()
     {
         ASNPtr result;
 
-        if (result = parseVariable())
+        if (result = parseLogical())
         {
             return result;
         }
-        else if (result = parseNumber())
+        else if (result = parsePrimary())
         {
             return result;
         }
@@ -155,19 +287,19 @@ ASNPtr parse(Vector<TokenPtr> const& tokens)
     return parser.parseProgram();
 }
 
-Exp Parser::parseExp()
-{
-    const ParseResult<Exp> result = parseExp(0);
-    // TODO: Why does result->tokenPos not work?
-    if (result->tokenPos == tokens->size()) {
-        return result->result;
-    } else {
-        throw new ParserException("Extra tokens starting at " + result->tokenPos);
-    }
-}
+//Exp Parser::parseExp()
+//{
+//    const ParseResult<Exp> result = parseExp(0);
+//    // TODO: Why does result->tokenPos not work?
+//    if (result->tokenPos == tokens->size()) {
+//        return result->result;
+//    } else {
+//        throw new ParserException("Extra tokens starting at " + result->tokenPos);
+//    }
+//}
 
-ParseResult::ParseResult(T&& result, std::size_t tokenPos)
-    : _result(std::move(result)), _tokenPos(tokenPos)
-{}
+//ParseResult::ParseResult(T&& result, std::size_t tokenPos)
+//    : _result(std::move(result)), _tokenPos(tokenPos)
+//{}
 
 } //namespace dflat
