f4949cc159cb5b9244e75d0f5d13163513e95f45
Rinzlo <joshuarco@gmail.com>
2019-02-26 00:47:52 -0800

    Update parser.cpp

--- src/parser.cpp
+++ src/parser.cpp
@@ -293,26 +293,22 @@ ASNPtr Parser::parseIfStmt()
     MATCH_(LeftBraceToken);
     PARSE(trueStatements, parseBlock());
     MATCH_(RightBraceToken);
-    
+    ASNPtr elseBlock;
     if( match<ElseToken>() )
     {
         MATCH_(LeftBraceToken);
         PARSE(falseStatements, parseBlock());
+        elseBlock = move(falseStatements);
         MATCH_(RightBraceToken);
-
-        CANCEL_ROLLBACK;
-        return make_unique<IfBlock>(
-            move(logicExp), 
-            move(trueStatements), 
-            move(falseStatements)
-            );
+    }else{
+        elseBlock = make_unique<Block>(Vector<ASNPtr>{});
     }
 
     CANCEL_ROLLBACK;
     return make_unique<IfBlock>(
         move(logicExp), 
         move(trueStatements), 
-        make_unique<Block>(Vector<ASNPtr>{})
+        move(elseBlock)
         );
 }
 
