25cb185a24c9e98aee133d0264eb7a488409423c
Brian McClelland <47439126+bmcclelland@users.noreply.github.com>
2019-02-25 21:14:01 -0800

    Added ASN comparison stuff

--- src/asn.hpp
+++ src/asn.hpp
@@ -26,7 +26,28 @@ public:
     virtual ~ASN();
     virtual String toString() const = 0;
     virtual ASNType getType() const = 0;
+    virtual bool cmp(ASN const&) const = 0;
+    
+    bool operator==(ASN const& other) const
+    {
+        if (getType() != other.getType())
+        {
+            return false;
+        }
+
+        return cmp(other);
+    }
 };
+
+// Do this once per ASN subclass.
+#define DECLARE_CMP(Type) \
+    bool cmp(ASN const& other) const override \
+    { \
+        return *this == static_cast<Type const&>(other); \
+    } \
+    /*end DECL_CMP*/
+
+
 
 using ASNPtr = std::unique_ptr<ASN>;
 
@@ -39,29 +60,50 @@ public:
     BinopExp(ASNPtr&& _left, OpType _op, ASNPtr&& _right);
     ASNType getType() const { return expBinop; }
     String toString() const;
+
+    bool operator==(BinopExp const& other) const
+    {
+        return left  == other.left
+            && op    == other.op
+            && right == other.right;
+    }
+    
+    DECLARE_CMP(BinopExp);
 };
 
 class VariableExp : public ASN
 {
-    public:
-        String name;
+public:
+    String name;
 
-        VariableExp(String const&);
-        ASNType getType() const { return expVariable; }
-        String toString() const;
+    VariableExp(String const&);
+    ASNType getType() const { return expVariable; }
+    String toString() const;
+
+    bool operator==(VariableExp const& other) const
+    {
+        return name == other.name;
+    }
+    
+    DECLARE_CMP(VariableExp);
 };
 
 class NumberExp : public ASN
 {
-    public:
-        int value;
-       
-        NumberExp(int);
-        ASNType getType() const { return expNumber; }
-        String toString() const;
-};
+public:
+    int value;
+   
+    NumberExp(int);
+    ASNType getType() const { return expNumber; }
+    String toString() const;
 
-    using ASNPtr = std::unique_ptr<ASN>;
+    bool operator==(NumberExp const& other) const
+    {
+        return value == other.value;
+    }
+    
+    DECLARE_CMP(NumberExp);
+};
 
 } //namespace dflat
 
