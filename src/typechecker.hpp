8194a440e53706aec6a8b3eba81801b66b7dba0b
Rinzlo <joshuarco@gmail.com>
2019-03-19 02:51:14 -0700

    Update typechecker.hpp

--- src/typechecker.hpp
+++ src/typechecker.hpp
@@ -11,38 +11,31 @@ namespace dflat
 TypeEnv typeCheck(Vector<ASNPtr> const&);
 Type typeCheck(ASNPtr const&);
 
-//Helper Functions:
-/**
- * Look up the rule for an operator based on cannonical name
- * Throw if not found.
- */ 
+#pragma region Helper Functions
+
+/** Look up the rule for an operator based on cannonical name
+ * Throw if not found. */ 
 Type lookupRuleType(TypeEnv const&, String const&);
-/**
- * Lookup the return type/arg type(s) of a method by name in current class.
- * Throw if method does not exist
- */ 
+
+/** Lookup the return type/arg type(s) of a method by name in current class.
+ * Throw if method does not exist */ 
 Vector<Type> lookupMethodType(TypeEnv const&, String const&);
-/**
- * Same as lookupMethodType, but with a specified class.
- */ 
+
+/** Same as lookupMethodType, but with a specified class. */ 
 Vector<Type> lookupMethodTypeByClass(TypeEnv const&, String const&, String const&);
-/**
- * Lookup the return type/arg type(s) of a variable by name in current class.
- * Throw if method does not exist
- */ 
+
+/** Lookup the return type/arg type(s) of a variable by name in current class.
+ * Throw if method does not exist */ 
 Type lookupVarType(TypeEnv const&, String const&);
-/**
- * Same as lookupVarType, but with a specified class.
- */ 
+
+/** Same as lookupVarType, but with a specified class. */ 
 Type lookupVarTypeByClass(TypeEnv const&, String const&, String const&);
-/**
- * Check if "type" is a valid type within the set of types.
- * Return true if valid, throw if not valid.
- */ 
+
+/** Check if "type" is a valid type within the set of types.
+ * Return true if valid, throw if not valid. */ 
 bool validType(TypeEnv const&, String const&);
-/**
- * Throw if two types aren't equal.
- */ 
+
+/** Throw if two types aren't equal. */ 
 void assertTypeIs(Type const&, Type const&);
 
 /// <summary>
@@ -53,18 +46,33 @@ void assertTypeIs(Type const&, Type const&);
 /// <param name="argTypes">The original function's parameters</param>
 /// <returns>name(arg_1, arg_2, ..., arg_n)</returns>
 String funcCanonicalName(String const&, Vector<Type> const&);
+
+/// <summary>
+/// Turns an operator into a function of the form op(rhsType)
+/// </summary>
+/// <param name="op">The operator (+, -, *, /)</param>
+/// <param name="rhsType">The variable (a number)</param>
+/// <returns>op(rhsType)</returns>
 String unopCanonicalName(OpType, Type const&);
+
+/// <summary>
+/// Turns an operator into a function of the form op(rhsType)
+/// </summary>
+/// <param name="op">The operator (+, -, *, /)</param>
+/// <param name="lhsType">The variable (a number)</param>
+/// <param name="rhsType">The variable (a number)</param>
+/// <returns>op({lhsType, rhsType})</returns>
 String binopCanonicalName(OpType, Type const&, Type const&);
 void mapNameToType(TypeEnv&, String const&, Vector<Type> const&);
 TypeEnv initialTypeEnv();
+
+#pragma endregion
 
 //TypeChecker Exceptions class:
 class TypeCheckerException : public std::exception
 {
     public:
-        /**
-         * TypeChecker Exception:
-         */ 
+        /** TypeChecker Exception: */ 
         TypeCheckerException(String msg) noexcept;
         const char* what() const noexcept;
     private:
