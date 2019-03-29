0d9f6d5002c76c90662fac6a6bfa6b6ec3b52679
Solid-Liquid <Solid-Liquid@users.noreply.github.com>
2019-03-28 18:50:48 -0700

    CodeGenerator Tests setup

---
+++ test/codegenerator_tests.cpp
@@ -0,0 +1,14 @@
+//Unit tests for Code Generation
+
+#include "catch2/catch.hpp"
+#include "codegenerator.hpp"
+
+using namespace dflat;
+
+TEST_CASE( "Expression Code Generation Tests", "[CodeGenerator]" )
+{
+    //DOES CODE GENERATION THROW EXCEPTIONS?? MY GUESS IS NO,
+    //ANY PROBLEMS WOULD BE HANDLED BEFOREHAND
+
+    REQUIRE( true == true);
+}
